3c7fccbba5cd0bc972fffddbf1baaa1a44d09319
black-wolf-cyber <51896348+alexeipc@users.noreply.github.com>
2022-07-25 22:02:32 +0700

    registration
    Haven't finished yet.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  get '/' => 'users#index'
+  #post '/sessions' => 'sessions#create'
+  post '/users' => 'users#create'
+  get '/dashboard' => 'dashboard#index'
+
   # root "articles#index"
 end
