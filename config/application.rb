9f51e8fe38462eac9651c15345f039536d94f891
DreamingPuma <107527017+DreamingPuma@users.noreply.github.com>
2022-08-08 18:41:55 +0700

    Update application.rb
    sets environment variables from local_env.yml file

--- config/application.rb
+++ config/application.rb
@@ -20,3 +20,10 @@ module VoiceCloningWeb
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
